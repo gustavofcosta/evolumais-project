a1cf82a329642efade85b137c670b65e17457f4e
Gustavo F Costa <gufercosta@gmail.com>
2022-10-05 18:07:18 -0300

    change font size

--- src/assets/styles/GlobalStyles.ts
+++ src/assets/styles/GlobalStyles.ts
@@ -1,28 +1,18 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-  /* fonts */
-  @import url('https://fonts.googleapis.com/css2?family=Cabin&family=Roboto+Condensed:wght@400;700&display=swap');
 
-  * {
+  *{
     margin: 0;
     padding: 0;
     outline: none;
     box-sizing: border-box;
   }
 
-
-  html {
-    font-family: Sans-Serif;
-  } /*16px*/
-
   body {
-    background: ${({ theme }) => theme.colors.backGroundColor};
-    font-family: ${({ theme }) => theme.fonts.bodyFont};
-    font-weight: 400;
-    line-height: 1.75;
+    font-family: sans-serif;
+    background-color: ${({ theme }) => theme.colors.backGroundColor};
     color: ${({ theme }) => theme.colors.textColor};
-    overflow-x: hidden;
   }
 
   p {
@@ -36,8 +26,7 @@ export default createGlobalStyle`
   h4,
   h5 {
     margin: 0;
-    margin-bottom: 1.38rem;
-    font-family: ${({ theme }) => theme.fonts.heading_font};
+    margin-bottom: 1.4rem;
     font-weight: 400;
     line-height: 1.3;
     text-transform: capitalize;
@@ -67,24 +56,16 @@ export default createGlobalStyle`
 
   small,
   .text-small {
-    font-size: ${({ theme }) => theme.text.small};
+  font-size: ${({ theme }) => theme.text.small};
   }
+
 
   a {
     text-decoration: none;
-    letter-spacing: ${({ theme }) => theme.text.letter_spacing};
   }
-  a,
-  button {
-    line-height: 1.15;
 
-  }
-  button:disabled {
-    cursor: not-allowed;
-  }
   ul {
     list-style-type: none;
-    padding: 0;
   }
 
   .img {
@@ -93,55 +74,12 @@ export default createGlobalStyle`
     object-fit: cover;
   }
 
-  /* buttons */
-  .btn {
-    cursor: pointer;
-    color: ${({ theme }) => theme.colors.white};
-    background: ${({ theme }) => theme.colors.primary_500};
-    border: transparent;
-    border-radius: ${({ theme }) => theme.border.sm};
-    letter-spacing: ${({ theme }) => theme.text.letter_spacing};
-    padding: 0.375rem 0.75rem;
-    box-shadow: ${({ theme }) => theme.shadow.sm};;
-    transition: ${({ theme }) => theme.transition.slow};
-    text-transform: capitalize;
-    display: inline-block;
-  }
-  .btn:hover {
-    background: ${({ theme }) => theme.colors.primary_700};
-    box-shadow: ${({ theme }) => theme.shadow.lg};
-  }
-  .btn-hipster {
-    color: ${({ theme }) => theme.colors.primary_500};;
-    background: ${({ theme }) => theme.colors.primary_500};
-  }
-  .btn-hipster:hover {
-    color: ${({ theme }) => theme.colors.primary_200};
-    background: ${({ theme }) => theme.colors.primary_700};
-  }
-  .btn-block {
-    width: 100%;
-  }
-  .btn-hero {
-    font-size: 1.25rem;
-    padding: 0.5rem 1.25rem;
-  }
-  .btn-danger {
-    background: ${({ theme }) => theme.colors.colorRedLight};
-    color: ${({ theme }) => theme.colors.colorGreenDark};
-  }
-  .btn-danger:hover {
-    background: ${({ theme }) => theme.colors.colorGreenDark};
-    color: ${({ theme }) => theme.colors.white};
-  }
-  /* alerts */
+
   .alert {
     padding: 0.375rem 0.75rem;
     margin-bottom: 1rem;
     border-color: transparent;
     border-radius: ${({ theme }) => theme.border.sm};
-    text-align: center;
-    letter-spacing: ${({ theme }) => theme.text.letter_spacing};
   }
 
   .alert-danger {
@@ -151,58 +89,6 @@ export default createGlobalStyle`
   .alert-success {
     color: ${({ theme }) => theme.colors.colorGreenDark};
     background: ${({ theme }) => theme.colors.colorGreenLight};
-  }
-
-  /* form */
-  .form {
-    width: 90vw;
-    max-width: ${({ theme }) => theme.width.fixed_width};
-    background: ${({ theme }) => theme.colors.white};
-    border-radius: ${({ theme }) => theme.border.sm};
-    box-shadow: ${({ theme }) => theme.shadow.md};
-    padding: 2rem 2.5rem;
-    margin: 3rem auto;
-    transition: ${({ theme }) => theme.transition.slow};
-  }
-  .form:hover {
-    box-shadow: ${({ theme }) => theme.shadow.xl};
-  }
-  .form-label {
-    display: block;
-    font-size: ${({ theme }) => theme.text.small};
-    margin-bottom: 0.5rem;
-    text-transform: capitalize;
-    letter-spacing: ${({ theme }) => theme.text.letter_spacing};
-  }
-  .form-input,
-  .form-textarea,
-  .form-select {
-    width: 100%;
-    padding: 0.375rem 0.75rem;
-    border-radius: ${({ theme }) => theme.border.sm};
-    background: ${({ theme }) => theme.colors.backGroundColor};
-    border: 1px solid ${({ theme }) => theme.colors.grey_200};
-  }
-  .form-input,
-  .form-select,
-  .btn-block {
-    height: 35px;
-  }
-  .form-row {
-    margin-bottom: 1rem;
-  }
-
-  .form-textarea {
-    height: 7rem;
-  }
-  ::placeholder {
-    font-family: inherit;
-    color: ${({ theme }) => theme.colors.grey_400};
-  }
-  .form-alert {
-    color: ${({ theme }) => theme.colors.colorRedDark};
-    letter-spacing: ${({ theme }) => theme.text.letter_spacing};
-    text-transform: capitalize;
   }
 
 
@@ -218,28 +104,14 @@ export default createGlobalStyle`
     border: 5px solid ${({ theme }) => theme.colors.grey_400};
     border-radius: 50%;
     border-top-color: ${({ theme }) => theme.colors.grey_500};
-    animation: spinner 2s linear infinite;
-  }
-  .loading-center {
-    margin: 0 auto;
+    animation: spinner 0.6s linear infinite;
   }
 
+  .loading {
+    display: flex;
+  }
 
-  /* title */
   .title {
     text-align: center;
-  }
-
-  .title-underline {
-    background: ${({ theme }) => theme.colors.grey_500};
-    width: 7rem;
-    height: 0.25rem;
-    margin: 0 auto;
-    margin-top: -1rem;
-  }
-
-  .full-page {
-    min-height: 100vh;
-    width: 100vw;
   }
 `;
